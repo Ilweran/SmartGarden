b09cd806a76f368295cf342afa6ed34cec722509
Eugene Smirnov <esmirnov@mirantis.com>
2019-09-12 18:05:56 +0400

    Started writing functions to calculate numeric sequencies

--- series.cpp
+++ series.cpp
@@ -2,7 +2,12 @@
  *	Author: Ilweran
  *	*/
 
+#include <chrono>
+#include <iostream>
+#include <ctime>
+using namespace std;
 
+#include "series.h"
 
 double calcNextValue(double x_cur)
 {
@@ -17,10 +22,28 @@ double getInitValue()
 	return x0;
 }
 
-void calcSeries(double x0)
+void calcSeries(double x0, double (*calcNextValue(double)))
 {
 	double x_cur = x0;
-	x_next = calcNextValue(x_cur);
-	double temp = 
+    double x_next = 0;
+    chrono::time_point<std::chrono::system_clock> start, end;
+    auto start = chrono::steady_clock::now();
+    int elapsed_seconds = 0;
+    
+    do
+    {
+      x_next = calcNextValue(x_cur);
+	  processCurrentValue(x_cur);
+      auto end = std::chrono::system_clock::now();
+      elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds> (end-start).count();
+    } while((abs(x_next - x_cur) > epsilon) || elapsed_seconds < 30)
+
+    printMessage("Divergent sequence? Press 's' to stop calculation.");
+    
+    while(не нажата любая кнопка)
+    {
+      x_next = calcNextValue(x_cur);
+      processCurrentValue(x_cur);
+    }
 }
 
