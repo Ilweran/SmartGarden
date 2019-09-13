85e0253ccf01efeaedc243be71231d5b612b0030
Eugene Smirnov <esmirnov@mirantis.com>
2019-09-13 17:31:14 +0400

    Implementing functions in series.* files P2

--- series.cpp
+++ series.cpp
@@ -2,6 +2,7 @@
  *	Author: Ilweran
  *	*/
 
+#include <cstring>
 #include <chrono>
 #include <iostream>
 #include <ctime>
@@ -9,6 +10,7 @@
 using namespace std;
 
 #include "series.h"
+#include "inputhandler.h"
 
 double calcNextValue(double x_cur)
 {
@@ -18,14 +20,20 @@ double calcNextValue(double x_cur)
 double getInitValue()
 {
 	double x0;
-	cout << "Input initial value x0: ";
+	cout << "Input initial value x0: "; //Add check for correctness of input!
 	cin >> x0;
 	return x0;
 }
 
-void calcSeries(double x0, double (*calcNextValue(double)))
+void processCurrentValue(double x)
 {
-	double x_cur = x0;
+  
+
+}
+
+void calcSeries(double x0, double calcNextValue(double))
+{
+  double x_cur = x0;
   double x_next = 0;
   chrono::time_point<std::chrono::system_clock> start, end;
   auto start = chrono::steady_clock::now();
@@ -34,17 +42,23 @@ void calcSeries(double x0, double (*calcNextValue(double)))
   do
   {
     x_next = calcNextValue(x_cur);
-	  processCurrentValue(x_cur);
+    processCurrentValue(x_cur);
     auto end = std::chrono::system_clock::now();
     elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds> (end-start).count();
   } while((abs(x_next - x_cur) > epsilon) || elapsed_seconds < 30)
 
   printMessage("Divergent sequence? Press any key to stop calculation.");
-    
+  
   while(!kbhit())
   {
     x_next = calcNextValue(x_cur);
     processCurrentValue(x_cur);
   }
+}
+
+void  printMessage(string str)
+{
+  LOCATE(40,1)
+  cout << str;
 }
 
--- series.h
+++ series.h
@@ -1,10 +1,17 @@
+/* Created 11 Sep 2019 by Ilweran */
 
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+double epsilon = 0.01;
 
 double calcNextValue(double);
 
 double getInitValue();
 
-void calcSeries(double, double (*f(double)));
+void calcSeries(double, double (*f)(double));
 
+void printMessage(string);
 
+void processCurrentValue(double);
